b2b4301990ca2417d458727002e1968d0c6c8784
falisa <zhudinghui@kingsware.cn>
2023-09-19 10:50:45 +0800

    init push

---
+++ index.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: 
+ * @Date: 2023-09-19 10:47:30
+ * @LastEditTime: 2023-09-19 10:50:38
+ * @LastEditors: sato-falisa
+ * @Description: 
+ * @FilePath: /demo/index.js
+ */
+document.getElementById("demo").innerHTML = 
+	typeof "john" + "<br>" + 
+	typeof 3.14 + "<br>" +
+	typeof false + "<br>" +
+	typeof [1,2,3,4] + "<br>" +
+	typeof {name:'john', age:34};
